b55353109cb8f9fd6c752a612984c985428c9f96
Subhabrata Paul <subhabratapaul277@gmail.com>
2025-04-27 02:44:12 +0530

    Create all methods

--- index.py
+++ index.py
@@ -17,18 +17,43 @@ def add_expense(description,amount,category):
     
 # for view expenses
 def view_expense():
-    pass
+    if not expenses:
+        print("No expenses found.")
+        return
+    for i, expense in enumerate(expenses, start=1):
+        print("-------------------------")
+        print(f"Expense {i}:")
+        print(f"Description: {expense.description}")
+        print(f"Amount: {expense.amount}")
+        print(f"Category: {expense.category}")
+        print("-------------------------")
+
 # calculataing total expenses
 def total_expense():
-    pass
+    total = 0
+    for expense in expenses:
+        total += expense.amount
+    print(f"Total expenses: Rs.{total:.2f}")  
+
 #Save expenses in file
 def save_expenses():
-    pass
+    with open("expenses.json", "w") as file:
+        data = [{"description": expense.description, "amount": expense.amount, "category": expense.category} for expense in expenses]
+        json.dump(data, file)
 #load in file
 def load():
-    pass
+    global expenses
+    try:
+        with open("expenses.json", "r") as file:
+            data = json.load(file)
+            for item in data:
+                expense = Expense(item["description"], item["amount"], item["category"])
+                expenses.append(expense)
+    except FileNotFoundError:
+        print("No previous data found.")
+
 def main():
-    
+    load() # Load previous data
     while True:
         print("\n1. Add Expense")
         print("2. View Expenses")
@@ -46,7 +71,7 @@ def main():
         elif choice == "2":
             view_expense()
         elif choice == "3":
-            total_expense
+            total_expense()
         elif choice == "4":
             save_expenses()
             print("Save successfully !!")
